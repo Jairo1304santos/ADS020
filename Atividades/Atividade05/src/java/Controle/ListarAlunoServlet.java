984eab0f5d6902491b58d6abf675e032e8e71473
Jairo1304santos <Jairo1304santos@gmail.com>
2020-03-25 23:48:17 -0300

    Código pronto, fazer apenas correção de erros

--- Atividades/Atividade05/src/java/Controle/ListarAlunoServlet.java
+++ Atividades/Atividade05/src/java/Controle/ListarAlunoServlet.java
@@ -1,5 +1,4 @@
-
-package Controle;
+package controle;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -17,77 +16,58 @@ import modelo.entidades.Aluno;
 @WebServlet(name = "ListarAlunoServlet", urlPatterns = {"/aluno"})
 public class ListarAlunoServlet extends HttpServlet {
 
-    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Aluno> lista = new ArrayList<Aluno>();
+
+        AlunoBO bo = new AlunoBO();
+        try {
+            lista = bo.listar();
+        } catch (NegocioException e) {
+            throw new ServletException("", e);
+        }
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            
+
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ListarAlunoServlet</title>");            
+            out.println("<title>Cadastro de Alunos</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet ListarAlunoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-  
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-      List<Aluno> lista = new ArrayList<Aluno>();
-      AlunoBO bo = new AlunoBO();
-       try {
-          lista = bo.listar();
-      } catch (NegocioException e) {
-          throw new ServletException("", e);
-      }
-      try (PrintWriter out = response.getWriter()) {
-            
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Cadastro de Alunos</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Listagem de Alunos</h1>");
+            out.println("<h1> Listagem de Alunos</h1");
+            out.println("<a href='" + response.encodeURL("/aluno/incluir") + "'>Incluir</a>");
             out.println("<table>");
             out.println("<tr>");
+            for (Aluno aluno : lista) {
+                out.println("<tr>");
+                out.println("<td>" + aluno.getMatricula() + "</td>");
+                out.println("<td>" + aluno.getNome() + "</td>");
+                out.println("<td><a href='" + response.encodeURL("/aluno/alterar?id=") + aluno.getId() + "'>Alterar</a>");
+                out.println("<a href='" + response.encodeURL("/aluno/excluir?id=") + aluno.getId() + "' onclick='return confirm('Deseja excluir o aluno?');'>Excluir</a></td>");
+                out.println("</tr>");
+            }
             out.println("<th>Matrícula</th>");
             out.println("<th>Nome</th>");
             out.println("<th>Ações</th>");
             out.println("</tr>");
-            
-            for (Aluno aluno: lista){
-            out.println("<tr>");
-            out.println("<td>" + aluno.getMatricula() + "</td>");
-            out.println("<td>" + aluno.getMatricula() + "</td>");
-            out.println("<td><a href=''>Alterar</a></td>");
-             out.println("<a href=''>Excluir</a></td>");
-            out.println("</tr>");
-            }
             out.println("</table>");
             out.println("</body>");
             out.println("</html>");
+
         }
-      
-     
-      
     }
 
-   
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        throw new ServletException("Método não suportado");
     }
 
-    
     @Override
     public String getServletInfo() {
         return "Short description";
